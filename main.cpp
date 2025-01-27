a3854fa5f9a5761fbccb44f94627f51a2a11429a
JohnBirmingham <jrbirmingham@ualr.edu>
2025-01-27 09:10:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+int menu() {
+  std::cout << "Welcome to number componder."<< std::endl;
+  std::cout << "1. Add a whole number to the system." << std::endl;
+  std::cout << "2. Print the numbers." << std::endl;
+  std::cout << "3. Double the numbers inside." << std::endl;
+  std::cout << "4. Add the numbers together." << std::endl;
+  std::cout << "5. 
+}
+int main() {
+  int menu;
+  menu = menu();
+  if(menu==1){
+    
+  }
+  else if(menu==2){
+    
+  }
+  else if(menu==3){
+    
+  }
+  else if(menu==4){
+    
+  }
+  else if(menu==5){
 
+  }
+  else if(menu==0){
+    
+  }
+  else {
+
+  }
+}
