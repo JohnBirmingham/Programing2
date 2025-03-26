19768f0cf3d11c7e5d34c61fefaed6136a67f673
JohnBirmingham <jrbirmingham@ualr.edu>
2025-03-26 13:18:46 -0500

    first practice 5 is done

--- practice/practice05/practice05_1a/practice05_1a.cpp
+++ practice/practice05/practice05_1a/practice05_1a.cpp
@@ -2,19 +2,30 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    std::vector<int> nums;
+    int input;
+    int newInput;
+    int sum=0;
+    std::cout << "Enter the amount of numbers you will be using: ";
+    std::cin >> input;
+    for (int i = 0; i < input; i++) {
+        std::cout << "Enter a number: ";
+        std::cin >> newInput;
+        nums.push_back(newInput);
+    }
+    for (int i = 0; i < nums.size(); i++) {
+        std::cout << nums.at(i) << ' ';
+    }
+    std::cout << '\n';
+    for (std::vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
+        if (*it%2 == 0) {
+           sum += *it;
+        }
+    }
+    std::cout << "The sum of the even numbers is: " << sum;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
