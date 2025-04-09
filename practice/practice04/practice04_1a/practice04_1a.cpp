a682db179fc614e35c55684db23335bcfc28fe9a
JohnBirmingham <jrbirmingham@ualr.edu>
2025-04-09 14:58:51 -0500

    changed practice04

--- practice/practice04/practice04_1a/practice04_1a.cpp
+++ practice/practice04/practice04_1a/practice04_1a.cpp
@@ -2,26 +2,30 @@
 //
 
 #include <iostream>
+double TempConversion(double temp, char type='F') {
+    double total;
+    if (type == 'C') {
+        total = (temp * 1.80) + 32;
+    }
+    else if (type == 'F') {
+        total = (temp - 32) * 1.80;
+    }
+    return total;
+}
 
 int main()
 {
-    int type;
+    char type;
     double total;
     double temp;
     while(true){
-        std::cout << "1. Convert Celsius to Fahrenheit" << std::endl;
-        std::cout << "2. Convert Fahrenheit to Celsius" << std::endl;
+        std::cout << "C. Convert Celsius to Fahrenheit" << std::endl;
+        std::cout << "F. Convert Fahrenheit to Celsius" << std::endl;
         std::cout << "Choose an option: " << std::endl;
         std::cin >> type;
         std::cout << "Choose a temperature: ";
         std::cin >> temp;
-        if (type == 1) {
-            total = (temp * 1.80) + 32;
-        }
-        else if (type == 2) {
-            total = (temp - 32) * 1.80;
-        }
+        total = TempConversion(temp,type);
         std::cout << "Converted: " << total << std::endl;
     }
-    
 }
